f48dd100f044e1d64ec5a168cb7f81f07b549657
GermannM3 <germannm@vk.com>
2025-01-28 03:55:37 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,18 +1,33 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-from aiogram import Bot
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
+import asyncio
 import os
 from dotenv import load_dotenv
+import logging
+from bot.database import init_db
+from bot.utils import publish_news
+from bot.handlers import router
 
-# Загрузка переменных окружения
 load_dotenv()
 
 app = FastAPI()
 bot = Bot(token=os.getenv("API_TOKEN"))
+dp = Dispatcher(storage=MemoryStorage())
+dp.include_router(router)
+
+logger = logging.getLogger(__name__)
 
 class MessageRequest(BaseModel):
     chat_id: str
     message: str
+
+@app.on_event("startup")
+async def startup_event():
+    init_db()
+    asyncio.create_task(dp.start_polling(bot, skip_updates=True))
+    asyncio.create_task(publish_news(bot))
 
 @app.post("/send_message")
 async def send_message(request: MessageRequest):
